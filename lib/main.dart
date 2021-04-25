442ea07cb7969166cdc02aed07fe6bd95ea318bf
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-04-25 22:01:25 +0100

    expense list impl

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,58 @@
+import 'package:expense_app/transactionListItem.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_app/transaction.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Expense Tracker',
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'new shoes',
+      amount: 99.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'new bag',
+      amount: 199.99,
+      date: DateTime.now(),
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Expense Tracker'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('chart'),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: transactions.map((transaction) {
+                return TransactionListItem(transaction: transaction);
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
---
+++ lib/transactionListItem.dart
@@ -0,0 +1,50 @@
+import 'package:expense_app/transaction.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TransactionListItem extends StatelessWidget {
+  final Transaction transaction;
+  TransactionListItem({
+    @required this.transaction,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 15,
+            ),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.purple, width: 2)),
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'N${transaction.amount}',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.purple),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                transaction.title,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                transaction.date.toString(),
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
